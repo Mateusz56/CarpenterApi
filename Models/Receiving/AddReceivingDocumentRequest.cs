eef1a16457670e3a80ad7d735fc87290b7281f73
Mateusz56 <mateusz.kozyra56@gmail.com>
2023-03-16 22:43:38 +0100

    Add validation to AddReceivingRequest

--- Models/Receiving/AddReceivingDocumentRequest.cs
+++ Models/Receiving/AddReceivingDocumentRequest.cs
@@ -2,9 +2,20 @@
 
 namespace CarpenterAPI.Models.Receiving
 {
-    public class AddReceivingDocumentRequest
+    public class AddReceivingDocumentRequest : IValidatableObject
     {
-        [Required, MinLength(1)]
+        [Required, MinLength(1, ErrorMessage = "Receiving document can not be empty.")]
         public ProductQuantity[] ProductQuantities { get; set;}
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            for (int i = 0; i < ProductQuantities.Length; i++)
+            {
+                if (ProductQuantities[i].ProductId == 0 || ProductQuantities[i].Quantity <= 0) {
+                    yield return new ValidationResult("Product and quantity must be provided.", new[] { nameof(ProductQuantities) });
+                    break;
+                }
+            }
+        }
     }
 }
